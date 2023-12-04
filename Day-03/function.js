296c85303716d9915a507807161fcbe9af3543b5
sagarshrestha81 <sagarcrestha81@gmail.com>
2023-12-04 10:08:04 +0545

    function

---
+++ Day-03/function.js
@@ -0,0 +1,5 @@
+a=10;
+function add(){
+    
+}
+add();
---
+++ Day-03/main.js
@@ -0,0 +1,43 @@
+a=5;
+// a="hello";
+// a=false;
+
+roll =[2,4,6,8,9];
+
+
+subjects=[23,"ram",1,true,80,24];
+// console.log(subjects);
+// subjects={'math':23,'com':45,'eng':66,'soc':12,'nep':40};
+
+// console.log(subjects);
+
+// console.log(typeof roll);
+
+// console.log(roll);
+// console.log(roll[3]);
+
+
+console.log(a);
+
+a=10
+console.log(a)
+
+console.log(roll)
+roll.push(30);
+console.log(roll)
+roll.pop();
+console.log(roll[2])
+subjects={
+    'id':23,
+    'name':"ram",
+    'absent':1,
+    'status':true,
+    'per':40,
+    'id':24
+    
+    };
+console.log(subjects.name)
+document.write(subjects.name.toUpperCase())
+
+
+
