d8d2a2bad3ea7cdc7e09949994ea2eb7ce5a6ec1
sagarshrestha81 <sagarcrestha81@gmail.com>
2023-12-19 18:25:13 +0545

    update day9

--- Day-01/main.js
+++ Day-01/main.js
@@ -1,2 +1,14 @@
 var a=90;
 console.log(a);
+ 
+ 
+console.log("hello2")
+console.log("hello2")
+console.log("hello2")
+console.log("hello2")
+console.log("hello2")
+console.log("hello2")
+console.log("hello2")
+console.log("hello2")
+console.log("hello2")
+console.log("hello2")
