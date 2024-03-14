3ffbb716295ef4c44aa99fe12281dd1cfb053acb
sagarshrestha81 <sagarcrestha81@gmail.com>
2024-03-14 09:15:12 +0545

    read php

---
+++ Day-12/layouts/footer.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "this is footer";
+function footer(){
+    
+}
---
+++ Day-13/server.php
@@ -0,0 +1,7 @@
+<?php
+
+$localhost="localhost";
+$username="root";
+$password="";
+$database="abc123";
+$conn=new mysqli($localhost,$username,$password,$database);
